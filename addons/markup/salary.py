075e42073036ba333cacc7c84d37e8c7f6182d77
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-12-19 11:50:52 +0300

    edit

--- addons/markup/salary.py
+++ addons/markup/salary.py
@@ -9,7 +9,7 @@ BACK_TO_MENU_BTN = KeyboardButton(text=MenuLexicon.BACK_MENU_BTN_TEXT)
 BACK_TO_RES_BTN = KeyboardButton(text=AccountantLexicon.BACK_TO_RES_BTN_TEXT)
 
 BACK_TO_EMPL_BTN = KeyboardButton(text=SalaryLexicon.BACK_TO_EMPL_BTN_TEXT)
-BACK_TO_TYPE_BTN = KeyboardButton(text=AccountantLexicon.BACK_TO_TYPE_BTN_TEXT)
+BACK_TO_TYPE_BTN = KeyboardButton(text=SalaryLexicon.BACK_TO_TYPE_BTN_TEXT)
 
 EMPL_MARKUP = ReplyKeyboardMarkup(
     keyboard=[[BACK_TO_RES_BTN], [BACK_TO_MENU_BTN]],
